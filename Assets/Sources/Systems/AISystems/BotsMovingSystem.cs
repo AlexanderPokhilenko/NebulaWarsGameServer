42b72aad0bf3c9043f5a729c6cdbc64dbfe4fec3
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2020-03-08 14:38:58 +0200

    Теперь точки для перемещения выбираются по формулам. Улучшилось чувство самосохранения.

--- Assets/Sources/Systems/AISystems/BotsMovingSystem.cs
+++ Assets/Sources/Systems/AISystems/BotsMovingSystem.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using Entitas;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public sealed class BotsMovingSystem : IExecuteSystem
 {
     private readonly GameContext gameContext;
     private IGroup<GameEntity> botsGroup;
+    private IGroup<GameEntity> withCannonGroup;
+    private IGroup<GameEntity> withDamageGroup;
     private const float zoneWarningDistance = 1.5f;
 
     public BotsMovingSystem(Contexts contexts)
@@ -15,6 +20,8 @@ public sealed class BotsMovingSystem : IExecuteSystem
         gameContext = contexts.game;
         var matcher = GameMatcher.AllOf(GameMatcher.Position, GameMatcher.Direction, GameMatcher.CircleCollider, GameMatcher.Bot).NoneOf(GameMatcher.TargetMovingPoint);
         botsGroup = gameContext.GetGroup(matcher);
+        withCannonGroup = gameContext.GetGroup(GameMatcher.AllOf(GameMatcher.Cannon, GameMatcher.Position, GameMatcher.Direction));
+        withDamageGroup = gameContext.GetGroup(GameMatcher.AllOf(GameMatcher.Damage, GameMatcher.Position, GameMatcher.Direction));
     }
 
     public void Execute()
@@ -28,6 +35,7 @@ public sealed class BotsMovingSystem : IExecuteSystem
             var zoneRadius = zone.circleCollider.radius;
 
             float maxRadius = e.circleCollider.radius + Random.Range(0, e.maxVelocity.value);
+            var sqrMaxRadius = maxRadius * maxRadius;
             Vector2 targetPosition = currentPosition + maxRadius * CoordinatesExtensions.GetRandomUnitVector2();
 
             //if (e.hasTargetingParameters)
@@ -43,9 +51,75 @@ public sealed class BotsMovingSystem : IExecuteSystem
             //    targetPosition += zonePosition;
             //}
 
+            if (e.hasMaxHealthPoints)
+            {
+                var dangerAvoidanceVector = new Vector2(0f, 0f);
+                // считаем здоровье, учитывая щиты и остальные объекты
+                var fullHp = e.GetAllChildrenGameEntities(gameContext, c => c.hasHealthPoints).Sum(c => c.healthPoints.value);
+                // может быть > 1, потому что считаем со щитами
+                var healthPercentage = fullHp / e.maxHealthPoints.value;
+                var combatPower = e.GetAllChildrenGameEntities(gameContext, c => c.hasCannon).Sum(GetEntityCombatPower);
+                combatPower *= healthPercentage;
+
+                var visionRadius = maxRadius + (e.hasTargetingParameters ? e.targetingParameters.radius : maxRadius);
+                var sqrVisionRadius = visionRadius * visionRadius;
+
+                dangerAvoidanceVector +=
+                    GetAvoidanceVector(withCannonGroup, cE => GetEntityCombatPower(cE) / combatPower);
+
+                dangerAvoidanceVector +=
+                    GetAvoidanceVector(withDamageGroup, cE => GetEntityDamagePower(cE) / fullHp);
+
+                if (dangerAvoidanceVector.sqrMagnitude > sqrMaxRadius)
+                {
+                    targetPosition += Vector2.ClampMagnitude(dangerAvoidanceVector, maxRadius);
+                }
+                else
+                {
+                    targetPosition += dangerAvoidanceVector;
+                }
+
+                /*static*/ float GetEntityCombatPower(GameEntity combatEntity)
+                {
+                    var cannon = combatEntity.cannon;
+                    var bullet = cannon.bullet;
+
+                    var power = bullet.collisionDamage;
+                    if (!bullet.isCollapses) power *= bullet.lifetime;
+                    power /= cannon.cooldown;
+
+                    return power;
+                }
+
+                /*static*/ float GetEntityDamagePower(GameEntity combatEntity)
+                {
+                    var damage = combatEntity.damage.value;
+                    if (!combatEntity.isCollapses) damage *= combatEntity.lifetime.value;
+                    return damage;
+                }
+
+                Vector2 GetAvoidanceVector(IGroup<GameEntity> dangerGroup, Func<GameEntity, float> calculatingFunc)
+                {
+                    var avoidanceVector = new Vector2(0f, 0f);
+                    foreach (var danger in dangerGroup)
+                    {
+                        if(danger.GetGrandOwnerId(gameContext) == e.GetGrandOwnerId(gameContext)) continue;
+                        var reverseDirection = targetPosition - danger.GetGlobalPositionVector2(gameContext);
+                        var sqrReverseDirection = reverseDirection.sqrMagnitude;
+                        if (sqrReverseDirection > sqrVisionRadius) continue;
+                        var dangerPercentage = calculatingFunc(danger);
+                        dangerPercentage *= 1f - sqrReverseDirection / sqrVisionRadius;
+                        avoidanceVector += reverseDirection.normalized * dangerPercentage;
+                    }
+
+                    return avoidanceVector;
+                }
+            }
+
+
             var safeRadius = zoneRadius - zoneWarningDistance;
 
-            if (targetPosition.sqrMagnitude >= safeRadius * safeRadius)
+            if ((targetPosition - zonePosition).sqrMagnitude >= safeRadius * safeRadius)
             {
                 var vectorToCenter = zonePosition - currentPosition;
                 var savingVector = Random.Range(zoneWarningDistance, maxRadius) * vectorToCenter.normalized;
