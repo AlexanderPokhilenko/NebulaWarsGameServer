2d95a4c072c7c0778b94b652b704679ad349ed96
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-04-03 19:00:26 +0300

    minor fixes

--- Assets/Sources/Components/IdComponent.cs
+++ Assets/Sources/Components/IdComponent.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Entitas;
+﻿using Entitas;
 using Entitas.CodeGeneration.Attributes;
-using UnityEngine;
 
 
 [Game, Input]
