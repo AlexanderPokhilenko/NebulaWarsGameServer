2c449f476801a6f4aad75e5056cf1c775b17c99e
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2020-07-23 21:46:48 +0300

    Добавлен метод для создания коллекторов, которые триггерятся на любые из перечисленных изменения.

---
+++ Assets/Scripts/ContextsCollectorsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Entitas;
+
+public static class ContextsCollectorsExtensions
+{
+    public static ICollector<TEntity> AnyChange<TContext, TEntity>(this TContext context, params IMatcher<TEntity>[] matchers)
+        where TContext : class, IContext<TEntity>
+        where TEntity : class, IEntity
+    {
+        return new Collector<TEntity>(
+            matchers.Select(m => context.GetGroup(m)).ToArray(),
+            matchers.Select(_ => GroupEvent.AddedOrRemoved).ToArray());
+    }
+}
